714814d07214378da40afacf5909d7828aa5e998
Chiriac Cristian <kiriac.cristian@gmail.com>
2020-01-10 23:13:16 +0200

    first commit

--- src/main/java/eu/accesa/springdemo/SpringDemoApplication.java
+++ src/main/java/eu/accesa/springdemo/SpringDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDemoApplication {
-
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
